e1a3ccc8b705d020183634fa00b34abb730d4923
Sviatoslav <mursvyats@gmail.com>
2024-02-09 00:15:45 +0500

    refact: deleted unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,18 @@
 'use client';
 
 import {useVisitorInfo} from "@/hooks/useVisitorInfo";
-import {QueryClient, QueryClientProvider, useQuery} from 'react-query';
 import {useUserAgent} from "@/hooks/useUserAgent";
 
-interface VisitData {
-    _id: string,
-    ip: string,
-    browser: string,
-    os: string,
-    device: string,
-    __v: number
-}
-
-const queryClient = new QueryClient();
-
 export default function Home() {
-    const ip = useVisitorInfo();
+    const {ipAddress, countryCode} = useVisitorInfo();
 
     const ua = useUserAgent();
 
     return (
         <>
             <h1>Your useragent is {ua}</h1>
-            <h1>IP {ip}</h1>
+            <h1>IP {ipAddress}</h1>
+            <h1>Country Code: {countryCode}</h1>
         </>
     );
 }
